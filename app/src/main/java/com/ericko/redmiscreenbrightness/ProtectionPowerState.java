5caac253c415d02b028fb79202bdf6b33f23fcdc
rabpitvn1-create <rabpitvn1@gmail.com>
2026-07-09 08:13:29 +0700

    Add protection power state model

---
+++ app/src/main/java/com/ericko/redmiscreenbrightness/ProtectionPowerState.java
@@ -0,0 +1,9 @@
+package com.ericko.redmiscreenbrightness;
+
+public enum ProtectionPowerState {
+    OFF,
+    ACTIVE_SCREEN_ON,
+    SCREEN_OFF_SLEEP,
+    USER_HOLD_LOW_POWER,
+    RECOVERY_WAKE
+}
